5e9b9f5b98a6ac031e8811f03dbac2fac793f45c
Ranggacaw <ranggacahyaw@gmail.com>
2025-03-06 10:49:16 +0700

    resize max-w di forgot password

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -33,7 +33,7 @@ const ForgotPassword = () => {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 font-serif">
-            <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
+            <div className="w-full max-w-sm p-6 bg-white rounded-lg shadow-md">
                 <h2 className="text-2xl font-semibold text-center text-gray-700">Lupa Password</h2>
                 {message && <p className="text-center text-green-500">{message}</p>}
                 <form onSubmit={handleResetPassword} className="mt-4">
